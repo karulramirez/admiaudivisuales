a575ed2dc1ab94467c822e6b9a178a0fbbf33679
karulramirez <43360854+karulramirez@users.noreply.github.com>
2021-10-28 23:46:08 -0500

    Add files via upload

---
+++ controlador/mostrarUsuarios.php
@@ -0,0 +1,33 @@
+<?php
+
+$variable = new Consultar();
+$sentencia= "SELECT * FROM usuarios";
+$filas = $variable->getDates($sentencia) ;
+//$consulta = $filas;
+//$resultados=mysqli_fetch_assoc($sentencia);
+/* var_dump($filas); */
+foreach($filas as $fila){
+
+    
+
+    echo "<tr>";
+    echo "<td>"; echo $fila['cedula']; echo "</td>";
+    echo "<td>"; echo $fila['nombre']; echo "</td>";
+    echo "<td>"; echo $fila['apellido']; echo "</td>";
+    echo "<td>"; echo $fila['facultad']; echo "</td>";
+    echo "<td>"; echo $fila['tel']; echo "</td>";
+    echo "<td>"; echo $fila['correo']; echo "</td>";
+    echo "<tr>";
+}
+
+
+/* while($filas=mysqli_fetch_assoc($resultados))
+{
+    echo "<tr>";//
+    echo "<td>"; echo $filas['sn']; echo "</td>";
+    echo "<td>"; echo $filas['ct']; echo "</td>";
+    echo "<td>"; echo $filas['modelo']; echo "</td>";
+    echo "<td>"; echo $filas['descripcion']; echo "</td>";
+    echo "<tr>";
+} */
+?>
